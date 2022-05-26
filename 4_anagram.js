0a681f34ed8efd3b4f9cf129b0bb2522b33f0d4a
Daniil-Stefaniv <Stefanivdanil@gmail.com>
2022-05-26 15:17:06 +0600

    add 4-quest

--- 4_anagram.js
+++ 4_anagram.js
@@ -4,24 +4,41 @@
  * @return {boolean}
  */
 function isAnagram(s, t) {
-  if (s.length !== t.length) return false
+  if ((1 > s > 5 * 104) || (s.length !== t.length)) return false
+  
+  s = s.toLowerCase()
+  t = t.toLowerCase()
   
   const review = []
-
-  s.split('').map(letter => {
+  
+  const arrT = t.split('')
+  const arrS = s.split('')
+  
+  arrS.map(letter => {
     review.push(letter)
+    
+    for (let i of arrT) {
+      if (letter !== i) continue
 
-    if(t.includes(letter)) review.pop(letter)
+      arrT.splice(arrT.indexOf(i), 1)
+      break
+    }
+
+    review.pop(letter)
   })
-
-  return review.length === 0 ? true : false
+  // console.log("arrT: ", arrT, "review: ", review)
+  return review.length === arrT.length ? true : false
 };
 
 
-console.log('quest-4: ', isAnagram('rat', 'tar')) // true
+console.log('quest-4: ', isAnagram('aab', 'abb')) // false
 console.log('quest-4: ', isAnagram('camelCase', 'Caelecasm')) // true
 console.log('quest-4: ', isAnagram('rat', 'tat')) // false
 console.log('quest-4: ', isAnagram('camelCase', 'Caelevasm')) // false
 console.log('quest-4: ', isAnagram('silent', 'listen')) // true
 console.log('quest-4: ', isAnagram('silent', 'listetrf')) // false
+console.log('quest-4: ', isAnagram('rt', 'tr')) // true
+console.log('quest-4: ', isAnagram('rrrtt', 'rrrrt')) // false
+console.log('quest-4: ', isAnagram('ytrewq', 'qwerty')) // true
+console.log('quest-4: ', isAnagram('ytrewq', 'qwertj')) // false
 
