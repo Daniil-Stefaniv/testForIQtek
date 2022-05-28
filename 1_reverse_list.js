0e834a91646a299b9884afc6bfe9bfafab7c687f
Daniil-Stefaniv <Stefanivdanil@gmail.com>
2022-05-28 13:30:49 +0600

    add 1-re-request

--- 1_reverse_list.js
+++ 1_reverse_list.js
@@ -8,40 +8,78 @@ function ListNode(val, next) {
  * @return {ListNode | null}
  */
 function reverseList(head) {
-  if(head === null || head.length === 0 || head[0] === null) return null
-  else if(head.length === 1 && head[0] !== null) return head
+  if (head === null) return null
 
-  const result = []
-
-  let peak = {
-    value: head[head.length - 1]
-  }
-
-  for (let i = head.length - 2; i >= 0; i--) {
-    // peak = { value: head[i] }
-    result.push(peak.value)
-    // if (i === 0) {
-    //   peak.next = null
-    // } 
-    
-    peak.next = {
-      value: head[i]
+  let tmp = head
+  let next = tmp.next
+  const peacks = []
+  while (tmp !== null) {
+    peacks.push(tmp)
+    tmp = next
+    next = tmp.next
+    if (next === null) {
+      peacks.push(tmp)
+      break
     }
-
-    peak = peak.next
-    
-    if(i === 0) result.push(peak.value)
   }
 
-  return result
+  peacks.reverse()
+
+  let result
+
+  peacks.map((peack, index) => {
+    nextPeack = peacks[index + 1]
+    if (index === peacks.length - 1) nextPeack = null
+    peack.next = nextPeack
+  })
+
+  return peacks[0]
 }
 
+const list = {
+  val: 1,
+  next: {
+    val: 2,
+    next: {
+      val: 3,
+      next: {
+        val: 4,
+        next: {
+          val: 5,
+          next: null
+        }
+      }
+    }
+  }
+}
 
-console.log( 'quest-1: ', reverseList([1, 2]) ) // [2, 1]
-console.log( 'quest-1: ', reverseList([1, 2, 3, 4, 5, 6, 7, 8, 9]) ) // [9, 8, 7, 6, 5, 4, 3, 2, 1]
-console.log( 'quest-1: ', reverseList([1, 2, 3, 4, 5, 6]) ) // [6, 5, 4, 3, 2, 1]
-console.log( 'quest-1: ', reverseList(null) ) // null
-console.log( 'quest-1: ', reverseList([]) ) // null
-console.log( 'quest-1: ', reverseList([1]) ) // [1]
-console.log( 'quest-1: ', reverseList([null]) ) // [null]
+const listTwo = {
+  val: 1,
+  next: {
+    val: 2,
+    next: null
+  }
+}
+
+const listThree = {
+  val: 1,
+  next: {
+    val: 2,
+    next: {
+      val: 3,
+      next: {
+        val: 26,
+        next: {
+          val: 145,
+          next: null
+        }
+      }
+    }
+  }
+}
+
+console.log('quest-1: ', reverseList(list)) // 5 -> 4 -> 3 -> 2 -> 1 -> null
+console.log('quest-1: ', reverseList(listTwo)) // 2 -> 1 -> null
+console.log('quest-1: ', reverseList(listThree)) // 145 -> 26 -> 3 -> 2 -> 1 -> null
+console.log('quest-1: ', reverseList(null)) // null
 console.log('')
--- 3_merge_two_lists.js
+++ 3_merge_two_lists.js
@@ -9,23 +9,13 @@ function ListNode(val, next) {
  * @return {ListNode | null}
  */
 function mergeLists(list1, list2) {
-  if (list1 === null && (list2 === null || list2.length === 0)) {
-    return null
-  } else if(list1 === null || list1.length === 0) {
-    if (list2 === null || list2.length === 0) return null 
-    
-    return list2.sort((a, b) => a - b)
-  } else if(list2 === null || list2.length === 0) {
-    return list1.sort((a, b) => a - b)
-  } 
-
-  return list1.concat(list2).sort((a, b) => a - b)
+  
 }
 
 
-console.log('quest-3: ', mergeLists([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6])) //[1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6]
-console.log('quest-3: ', mergeLists(null, [0])) //[0]
-console.log('quest-3: ', mergeLists([1, 2, 3, 904, 78, 120], null)) //[1, 2, 3, 78, 120, 904]
-console.log('quest-3: ', mergeLists([4, 5, 0, 56, 9], [3])) //[0, 3, 4, 5, 9, 56]
-console.log('quest-3: ', mergeLists([], [])) //null
-console.log('')
+// console.log('quest-3: ', mergeLists([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6])) //[1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6]
+// console.log('quest-3: ', mergeLists(null, [0])) //[0]
+// console.log('quest-3: ', mergeLists([1, 2, 3, 904, 78, 120], null)) //[1, 2, 3, 78, 120, 904]
+// console.log('quest-3: ', mergeLists([4, 5, 0, 56, 9], [3])) //[0, 3, 4, 5, 9, 56]
+// console.log('quest-3: ', mergeLists([], [])) //null
+// console.log('')
